d1a83d106fe9d47c8fd2fd5bb541e244019da1e9
techsinno0-spec <techsinno0@gmail.com>
2026-07-14 09:05:57 +0200

    Bundle Tabler icons locally and tighten CSP for offline assets

--- web-server.js
+++ web-server.js
@@ -10,7 +10,8 @@ const WEB_DIR = path.join(__dirname, 'web');
 const MIME = {
   '.html': 'text/html', '.css': 'text/css', '.js': 'application/javascript',
   '.json': 'application/json', '.png': 'image/png', '.jpg': 'image/jpeg',
-  '.svg': 'image/svg+xml', '.ico': 'image/x-icon', '.woff2': 'font/woff2', '.woff': 'font/woff'
+  '.svg': 'image/svg+xml', '.ico': 'image/x-icon', '.woff2': 'font/woff2', '.woff': 'font/woff',
+  '.ttf': 'font/ttf'
 };
 
 const server = http.createServer((req, res) => {
